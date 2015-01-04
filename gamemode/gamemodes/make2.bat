14f2c3c552957753a32a765e5499e532aa183b35
eider-i128 <eider-i128@users.noreply.github.com>
2015-01-04 23:52:24 +0100

    Aktualizacja skryptu do kompilacji pod Windows

--- gamemode/gamemodes/make2.bat
+++ gamemode/gamemodes/make2.bat
@@ -2,20 +2,24 @@
 
 time /T
  
-echo #define GMVERSION "GITr2981" > ../include/fullserver/version.inc
+echo #define GMVERSION "PUBLICrGIT" > ../include/fullserver/version.inc
 
-set KIEDY=date /T
+setlocal enabledelayedexpansion
+FOR /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set DATE=%%c-%%a-%%b)
+FOR /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set TIME=%%a:%%b)
+
 set USER=none
 set GDZIE=builder
 set GMHOST=127.0.0.1
 set GMPORT=7777
 
-echo #define GMCOMPILED "skompilowana %KIEDY% przez %USER%@%GDZIE%" >> ../include/fullserver/version.inc
+echo #define GMCOMPILED "skompilowana %DATE% %TIME% przez %USER%@%GDZIE%" >> ../include/fullserver/version.inc
 echo #define GMHOST "%GMHOST%" >> ../include/fullserver/version.inc
 echo #define GMPORT %GMPORT% >> ../include/fullserver/version.inc
 echo #define SERVER_NUM 1 >> ../include/fullserver/version.inc
+endlocal
 
 echo Please wait...
-..\pawno\pawncc.exe -A4 -v2 -d2 -i..\\include -i..\\include_3z fs.pwn  -\;\+ -\\ -\(\+ > build.log
+..\pawno\pawncc.exe -A4 -v2 -d2 -i..\\include -i..\\include_3z fs.pwn  -\;\+ -\\ -\(\+
 time /T
 pause
